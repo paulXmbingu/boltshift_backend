106c2a04856bda5b9fe92d36a75ca8d3864018d3
mkyd-kill <romeomureithi@gmail.com>
2023-11-05 18:34:45 +0300

    added token urls

--- customer/urls.py
+++ customer/urls.py
@@ -1,3 +1,8 @@
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, # obtaining a token
+    TokenRefreshView, # refreshing the obtained token
+    TokenVerifyView # verifying the obtained token
+)
 from django.urls import path
 from . import views
 from rest_framework import routers
@@ -6,6 +11,11 @@ router = routers.DefaultRouter()
 router.register("sign-up", views.CustomerRegistration, 'sign-up')
 
 urlpatterns = [
+    # tokens
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     path('login/', views.CustomerLogin.as_view(), name='login'),
     path('delete-account/<str:cid>/soft-delete', views.CustomerDeleteAccount.as_view(), name='delete'),
     path('logout', views.CustomerLogout.as_view(), name='logout'),
