e73e809fe80dc18e40fd72bb1f5557c0e717d9bc
mkyd-kill <romeomureithi@gmail.com>
2023-11-05 16:16:58 +0300

    changed token from knox to rest_simplejwt

--- boltshift/settings.py
+++ boltshift/settings.py
@@ -41,13 +41,12 @@ INSTALLED_APPS = [
     'product',
     'vendors',
 
-    # API handling
+    # REST API handling
     'rest_framework',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 
-    # API Token Authentication
-    'knox',
-
-    # React Data Handling
+    # React API Data Handling
     'corsheaders'
 ]
 
@@ -74,11 +73,44 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-        'knox.auth.TokenAuthentication'
-    ]
+    # authentication classes
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+# REST Framework JWT Authentication
+SIMPLE_JWT = {
+        'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=20),
+        'ROTATE_REFRESH_TOKENS': True,
+        'BLACKLIST_AFTER_ROTATION': True,
+        'UPDATE_LAST_LOGIN': False,
+
+        # encryption algorithm
+        'ALGORITHM': 'HS256',
+
+        'VERYFYING_KEY': None,
+        'AUDIENCE': None,
+        'ISSUER': None,
+        'JWK_URL': None,
+        'LEEWAY': 0,
+
+        'AUTH_HEADER_TYPES': ('Bearer', ),
+        'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+        'USER_ID_FIELD': 'id',
+        'USER_ID_CLAIM': 'user_id',
+        'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_authentication_rule',
+
+        'AUTH_TOKEN_CLASSES': [
+            'rest_framework_simplejwt.tokens.AccessToken'
+        ],
+        'TOKEN_TYPE_CLAIM': 'token_type',
+        'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+        'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+        'SLIDING_TOKEN_LIFETIME': timedelta(minutes=10),
+        'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
 # Backend POST server authentication
@@ -193,7 +225,8 @@ JAZZMIN_SETTINGS = {
     # adding icons to the dashboard
     "icons": {
         "auth": "fas fa-users-cog", # default auth token icon
-        "knox.authtoken": "fa fa-lock", # knox auth icon
+        'token_blacklist.blacklistedtoken': "fa fa-lock", # rest_framework auth token icon
+        'token_blacklist.outstandingtoken': "fa fa-lock",
         "auth.user": "fas fa-user", # user icon
         "auth.Group": "fas fa-users", # group icon
 
