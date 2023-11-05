b1d210012db5b188531aa7c2a80a734bd4288229
mkyd-kill <romeomureithi@gmail.com>
2023-11-05 17:27:47 +0300

    updated serializer

--- vendors/serializer.py
+++ vendors/serializer.py
@@ -1,27 +1,13 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from .models import Vendor
-from hashed import hash_password
 
-# password min length
-MIN_LENGTH = 8
 # username min_length
 LENGTH = 6
 
 class VendorRegistrationAPI(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only = True,
-        min_length = MIN_LENGTH,
-        error_messages = {
-            "min_length": f"Passwords must be {MIN_LENGTH} characters long"
-        }
-    )
-    password2 = serializers.CharField(
-        write_only = True,
-        min_length = MIN_LENGTH,
-        error_messages = {
-            "min_length": f"Passwords must be {MIN_LENGTH} characters long"
-        }
-    )
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    confirm_password = serializers.CharField(write_only=True, required=True)
     username = serializers.CharField(
         min_length = LENGTH,
         error_messages = {
@@ -34,13 +20,11 @@ class VendorRegistrationAPI(serializers.ModelSerializer):
         exclude = ['first_name', 'last_name', 'image', 'warranty_period', 'response_time', 'shipping_time', 'user', 'address', 'rating', 'groups', 'user_permissions', 'is_staff', 'is_superuser', 'last_login', 'date_joined']
 
     def validate(self, data):
-        if data["password"] != data["password2"]:
+        if data["password"] != data["confirm_password"]:
             raise serializers.ValidationError("Passwords do not match")
         return data
     
     def create(self, validated_data):
-        password_hashed = hash_password(validated_data["password"], validated_data["password2"])
-
         vendor = Vendor.objects.create(
             vendor_name = validated_data["vendor_name"],
             email = validated_data["email"],
@@ -48,7 +32,7 @@ class VendorRegistrationAPI(serializers.ModelSerializer):
             username = validated_data["username"],
             warranty_period = 0
         )
-        vendor.set_password(password_hashed)
+        vendor.set_password(validated_data["password"])
 
         vendor.save()
 
