43699098dbc63dbd287a584f45c1520abce0d484
Maingi <layersony@gmail.com>
2024-09-16 11:53:33 +0300

    fix: dockerized application

--- db/scripts/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the backup file name with a timestamp
-BACKUP_FILE="db/scripts/db_backup_$(date +%Y%m%d%H%M%S).sqlite3"
-
-# Copy the SQLite database to the backup directory
-cp /app/db.sqlite3 $BACKUP_FILE
-
-echo "Database backup completed: $BACKUP_FILE"
