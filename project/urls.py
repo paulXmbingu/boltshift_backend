ba2dd3e91485550246115ecf929c3a42aea5610b
Paul <paulXmbingu@gmail.com>
2025-02-17 03:54:39 +0300

    Update

--- project/urls.py
+++ project/urls.py
@@ -3,11 +3,10 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns = [    
     path('admin/', admin.site.urls),
     path('customer/', include('customer.urls')),
-    path('', include('product.urls')),
+    path('product/', include('product.urls')),
     path('vendor/', include('vendors.urls')),
     path('provision/', include('provision.urls')),
 
