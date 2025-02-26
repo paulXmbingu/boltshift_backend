3ce4f40f90d10574360803919cb7cb9aba9327f3
Paul <paulXmbingu@gmail.com>
2025-02-26 05:28:38 +0300

    Update

--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -1,8 +1,4 @@
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, # obtaining a token
-    TokenRefreshView, # refreshing the obtained token
-    TokenVerifyView # verifying the obtained token
-)
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
 from django.shortcuts import redirect
 from django.urls import path
 from . import views
@@ -11,20 +7,16 @@ def redirect_url(request):
     return redirect("/product/")
 
 urlpatterns = [
-    # default url redirects to the general product page
     path('', redirect_url),
     
-    # tokens authentication and verification for user account authentication
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     
-    # user authentication
     path("sign-up/", views.CustomerRegistration.as_view(), name='sign-up'),
     path('login/', views.CustomerLogin.as_view(), name='login'),
     path('logout', views.CustomerLogout.as_view(), name='logout'),
-    
-    # user account
+ 
     path('delete-account', views.CustomerDeleteAccount.as_view(), name='delete'),
     path('checkout', views.CustomerCheckout.as_view(), name='checkout'),
     path('account-settings/', views.CustomerAccountSettings.as_view(), name='account'),
