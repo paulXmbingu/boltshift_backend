bac2beb9d9aa8f408ad22fb7b7e38413e905ad74
Paul <paulXmbingu@gmail.com>
2025-02-27 00:25:01 +0300

    Update

--- apps/customer/urls.py
+++ apps/customer/urls.py
@@ -12,11 +12,9 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    
     path("sign-up/", views.CustomerRegistration.as_view(), name='sign-up'),
     path('login/', views.CustomerLogin.as_view(), name='login'),
     path('logout', views.CustomerLogout.as_view(), name='logout'),
- 
     path('delete-account', views.CustomerDeleteAccount.as_view(), name='delete'),
     path('checkout', views.CustomerCheckout.as_view(), name='checkout'),
     path('account-settings/', views.CustomerAccountSettings.as_view(), name='account'),
